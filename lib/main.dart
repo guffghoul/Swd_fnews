adc697b155eb1be6bb05a505f6b3ba5f304248cc
nguyenchien789 <52992835+nguyenchien789@users.noreply.github.com>
2020-06-14 16:31:37 +0700

    update ui channel

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:swdmobileapp/screens/login.dart';
 
+import 'screens/channel.dart';
+
 void main() => runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: LoginPageWidget()
+    home: ChannelWidget()
   ));
